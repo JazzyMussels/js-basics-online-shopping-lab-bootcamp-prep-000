9a277ee2c79a6041aab2830736b52fc59e630b9b
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-10 19:55:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,9 +42,10 @@ function viewCart() {
 }
 
 function total() {
-  for (var i = 0; i < cart.length; i +=1 )
+  for (var i = 0; i < cart.length; i +=1 ) {
     var counter = 0 
   counter += cart[i]['itemPrice']
+  }
   return counter
 }
 
