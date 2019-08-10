274c6050af3447a0194631c0db0e1a6cd1fbbc47
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-10 19:23:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,9 +24,11 @@ function viewCart() {
   if (cart.length === 0) {
     return 'Your shopping cart is empty.' 
   } else if  (cart.length === 1) {
-    var msg = (`In your cart, you have ${cart[i]['itemName']} at $${cart[i]['itemPrice']}.`)
-    return msg
-  } 
+    return (`In your cart, you have ${cart[0]['itemName']} at $${cart[0]['itemPrice']}.`)
+  }  else 
+  var msg = `In your cart, you have `
+      for (var i = 0; i < cart.length; i +=1)
+       msg + `${cart[i]['itemName']} at ${cart[i]['itemPrice']}, and `
       
     
 }
