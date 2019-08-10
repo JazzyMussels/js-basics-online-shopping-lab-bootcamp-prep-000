32a522ee272ed01fd6f798c4d921bd83f3e781f0
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-10 20:43:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,10 +60,10 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (placeOrder(cardNumber) === undefined) {
-    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
+  if (cardNumber === undefined) {
+     return "Sorry, we don't have a credit card on file for you."
   } else {
-    return "Sorry, we don't have a credit card on file for you."
+   return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
   }
   
 }
