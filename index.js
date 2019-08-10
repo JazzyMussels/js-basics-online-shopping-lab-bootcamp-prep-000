b17e124aed22118fd998eba7e6fe0f798382d1e4
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-10 20:37:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  
-  
-  return "Sorry, we don't have a credit card on file for you."
+  if (placeOrder()) {
+    return "Sorry, we don't have a credit card on file for you."
+  }
 }
