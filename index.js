d110b08162919c3fab84ae18281126626a47c5b3
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-10 19:08:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,11 +21,15 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  if (cart.length === 0)
-    return 'Your shopping cart is empty.'
-  else
-  for (var i = 0; i < cart.length; i +=1)
+  if (cart.length === 0) {
+    return 'Your shopping cart is empty.' 
+  } else if  (cart.length === 1) {
     return(`In your cart, you have ${cart[i]['itemName']} at $${cart[i]['itemPrice']}.`)
+  }  else 
+      for (var i = 0; i < cart.length; i +=1)
+       return(`In your cart, you have ${cart[i]['itemName']} at $${cart[i]['itemPrice']}.`)
+      
+    
 }
 
 function total() {
